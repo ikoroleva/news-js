3da32747bea4293a752232dcbeed9b8d2226a679
i.koroleva <prsml.ellisson@gmail.com>
2023-01-28 15:28:52 +0100

    feat:install libs

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: '52cb939de55e4ea4ba15d4dd9b7193b2',
+    });
+  }
 }
 
 export default AppLoader;
